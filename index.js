2b023bac7c2a63f4cdf8e437c3f4cb5b62db5c7b
Denis4pf <denis.onwonga@student.moringaschool.com>
2022-08-05 09:13:47 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+
+const element = document.getElementById("main")
+element.remove();
+
+var newHeader = document.createElement('h1')
+newHeader.setAttribute('id', 'victory');
+newHeader.innerHTML = "Joseph is the champion"
